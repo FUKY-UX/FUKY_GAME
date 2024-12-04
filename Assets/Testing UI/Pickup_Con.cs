f078c4be65788c6ac6a378510ba76af41a85bef5
TheSausageDog <1972520730@qq.com>
2024-12-04 23:20:11 +0800

    相机测试场景以及一个抓取脚本

---
+++ Assets/Testing UI/Pickup_Con.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Con : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdpos;
+
+}
