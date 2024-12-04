8d75e61cd75e28d213716e476438e7cee6bc7318
TheSausageDog <1972520730@qq.com>
2024-12-04 21:17:34 +0800

    优化后的UI控制器和外部端口
    我是瑞恩高斯林，我用gpt优化代码

---
+++ Assets/Testing UI/脚本/Object_Controller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Controller : MonoBehaviour
+{
+    // 是否允许自动增加HP
+    public bool 允许自动增加 = true;
+
+    // HP增长速率（每秒增加多少HP）
+    public float 成熟度增长速率 = 10f;
+
+    // 你可以根据需要添加更多控制参数或方法
+}
